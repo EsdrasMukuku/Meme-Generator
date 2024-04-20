bf564a4c20ae7033c7ffc6a726e9e38020f70adf
EsdrasMukuku <esdrasbam0@gmail.com>
2024-04-19 20:13:17 -0500

    added images

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,37 @@
+import { useState } from "react"
+import memesData from "./memesData"
+
 
 
 const Form = () => {
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemeImages, setAllMemeImages] = useState(memesData)
+    function getMemeImage(){
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    }
   return (
-    <form className="h[170px]  flex flex-col items-center my-10">
+    <main>
+    <div className="h[170px]  flex flex-col items-center my-10">
         <div className="grid grid-cols-2 w-[477px]  ">
         <label htmlFor="first" className=" text-black font-medium ">Top text</label>
         <label htmlFor="second" className="ml-5 text-black font-medium ">Bottom text</label>
-      <input type="text" name="first" id="first" className="border w-[220px] h-[34px] border-lightGray rounded pl-3 text-gray"/>
-      <input type="text" name="second" id="second" className="border w-[220px] h-[34px] border-lightGray rounded ml-5 pl-3 text-gray"/>
+      <input type="text" name="first" id="first" className="top-text border w-[220px] h-[34px] border-lightGray rounded pl-3 text-gray "/>
+      <input type="text" name="second" id="second" className="bottom-text border w-[220px] h-[34px] border-lightGray rounded ml-5 pl-3 text-gray"/>
       </div>
-      <button type="submit" className="w-[477px] text-center text-white h-[40px] rounded bg-gradient-to-r from-darkPurple to-lightPurple mt-4">Get a new meme image</button>
-    </form>
+      <button  onClick={getMemeImage} type="submit" className="w-[477px] text-center text-white h-[40px] rounded bg-gradient-to-r from-darkPurple to-lightPurple mt-4 hover:opacity-75">Get a new meme image</button>
+    </div>
+    <img src={meme.randomImage} alt="" className="w-[477px] h-[268px] ml-10 "/>
+    </main>
   )
 }
 
